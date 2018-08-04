c1de91e2facf19c6e8efedaadf49d4a10983f7bf
Jesse <brennan@pacific.net>
2018-08-03 17:01:14 -0700

    Add 2/3 testing and support

--- bouncer/bouncer.py
+++ bouncer/bouncer.py
@@ -1,7 +1,7 @@
-
 import json
-
 import boto3
+
+import six
 from botocore.exceptions import ClientError
 
 
@@ -20,18 +20,20 @@ class Bouncer(object):
     session = boto3.session.Session()
     client = session.client(service_name='secretsmanager', endpoint_url=SECRETS_MANAGER_URL)
 
-    def __init__(self, secret_name: str):
+    def __init__(self, secret_name):
         """
         :param secret_name: The name of the secret in
+        :type secret_name: str
         the AWS Secret Manager. For example: commons/dev/whitelist
         """
         self.secret_name = secret_name
 
-    def is_authorized(self, email: str) -> bool:
+    def is_authorized(self, email):
         """
         Identify if the given email address is included in the email whitelist.
 
         :param email: Google email address for which authorization is to be determined
+        :type email: str
         :return: True if the email address is authorized, False if not authorized
         :exception SecretManagerException: If an error occurs.
         """
@@ -42,11 +44,12 @@ class Bouncer(object):
             get_secret_value_response = self.client.get_secret_value(SecretId=self.secret_name)
         except ClientError as e:
             if e.response['Error']['Code'] == 'ResourceNotFoundException':
-                raise SecretManagerException("The requested secret " + self.secret_name + " was not found") from e
+                six.raise_from(SecretManagerException("The requested secret {} was not found"
+                                                      .format(self.secret_name)), e)
             elif e.response['Error']['Code'] == 'InvalidRequestException':
-                raise SecretManagerException("The request was invalid due to:", e) from e
+                six.raise_from(SecretManagerException("The request was invalid due to:", e), e)
             elif e.response['Error']['Code'] == 'InvalidParameterException':
-                raise SecretManagerException("The request had invalid params:", e) from e
+                six.raise_from(SecretManagerException("The request had invalid params:", e), e)
             else:
                 raise
         else:
@@ -54,6 +57,6 @@ class Bouncer(object):
             secret_dict = json.loads(secret_string)
             try:
                 return secret_dict[SECRET_KEY].split(',')
-            except KeyError as e:
+            except KeyError:
                 raise SecretManagerException("Your secret is misformatted. Expected key: {}, actually found: {}"
                                              .format(SECRET_KEY, list(secret_dict.keys())))
